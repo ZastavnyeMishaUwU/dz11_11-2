c247f2cd920d66c4fc0e6de60f308cfb2bd9f233
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-11-13 20:54:54 +0200

    Add project files.

---
+++ dz11_11-2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace dz11_11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 12, 25, 33, 18, 40 };
+            int minRange = 10;
+            int maxRange = 30;
+
+            Console.Write("Числа в діапазоні 10-30: ");
+            bool first = true;
+
+            foreach (int number in array)
+            {
+                if (number >= minRange && number <= maxRange)
+                {
+                    if (!first)
+                    {
+                        Console.Write(", ");
+                    }
+                    Console.Write(number);
+                    first = false;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
